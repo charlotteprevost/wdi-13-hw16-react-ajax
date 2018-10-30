0f8f484d9e99b97bf5c507f88080cea9fde7d465
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-30 17:24:00 -0500

    Created app - requesting Login Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      logged: false,
+      username: ''
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img alt="Pokémon" width="500px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2000px-International_Pok%C3%A9mon_logo.svg.png"/>
         </header>
+        <Login />
       </div>
     );
   }
